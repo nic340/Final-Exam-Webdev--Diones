b9df17936eaa7f3aadf59ffd7e39ea17c73f7221
Prime Christian S. Neri <neri.primechristian@gmail.com>
2025-12-17 06:35:49 -0800

    add

---
+++ front_files/script.js
@@ -0,0 +1,17 @@
+const API_URL = "http://127.0.0.1:8000/brainrots";
+
+async function loadBrainrots() {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+
+  const list = document.getElementById("brainrot-list");
+  list.innerHTML = "";
+
+  data.forEach(item => {
+    const li = document.createElement("li");
+    li.textContent = `${item.title} - ${item.description} (Score: ${item.score})`;
+    list.appendChild(li);
+  });
+}
+
+loadBrainrots();
