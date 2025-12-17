aef70ad340beb890d4198aba55ffe69d16d1c594
Prime Christian S. Neri <neri.primechristian@gmail.com>
2025-12-17 09:12:41 -0800

    changes

--- back_files/models.py
+++ back_files/models.py
@@ -1,8 +1,7 @@
 from sqlmodel import SQLModel, Field
-from typing import Optional
 
 class Brainrot(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: int = Field(default=None, primary_key=True)
     title: str
-    description: str
-    score: int
+    content: str
+    image: str
