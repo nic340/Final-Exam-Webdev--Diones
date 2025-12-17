8b8fec84168cded39ea43d6d45a224ec9d6cdec6
Prime Christian S. Neri <neri.primechristian@gmail.com>
2025-12-17 05:05:21 -0800

    Create SQLModel models and SQLite database

---
+++ back_files/database.py
@@ -0,0 +1,9 @@
+from sqlmodel import SQLModel, create_engine
+
+
+sqlite_url = "sqlite:///database.db"
+engine = create_engine(sqlite_url, echo=True)
+
+
+def create_db():
+SQLModel.metadata.create_all(engine)
