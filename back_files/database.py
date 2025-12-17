cef814975a5937d2f6e391f50b285bdf94713df9
Prime Christian S. Neri <neri.primechristian@gmail.com>
2025-12-17 09:08:09 -0800

    changes

--- back_files/database.py
+++ back_files/database.py
@@ -2,5 +2,5 @@ from sqlmodel import SQLModel, create_engine
 
 engine = create_engine("sqlite:///database.db", echo=True)  
 def init_db():
-    from models import Post
+    from models import Brainrot
     SQLModel.metadata.create_all(engine)
