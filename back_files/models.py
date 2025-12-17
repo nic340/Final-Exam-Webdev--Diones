e3e06da78e080731885f4b2eba51972aab804de0
Prime Christian S. Neri <neri.primechristian@gmail.com>
2025-12-17 05:37:15 -0800

    Add SQLModel Brainrot model and SQLite database configuration

--- back_files/models.py
+++ back_files/models.py
@@ -1,9 +1,8 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 
-
 class Brainrot(SQLModel, table=True):
-id: Optional[int] = Field(default=None, primary_key=True)
-title: str
-description: str
-score: int
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    description: str
+    score: int
