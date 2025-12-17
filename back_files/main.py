847b562fed9338179fd0bcd8f7ab68dbca2726fc
Prime Christian S. Neri <neri.primechristian@gmail.com>
2025-12-17 05:42:33 -0800

    Implement GET and POST endpoints for Brainrot API

--- back_files/main.py
+++ back_files/main.py
@@ -3,27 +3,23 @@ from sqlmodel import Session, select
 from database import engine, create_db
 from models import Brainrot
 
-
 app = FastAPI()
-
 
 @app.on_event("startup")
 def on_startup():
-create_db()
-
+    create_db()
 
 @app.get("/brainrots")
 def get_brainrots():
-with Session(engine) as session:
-statement = select(Brainrot)
-results = session.exec(statement).all()
-return results
-
+    with Session(engine) as session:
+        statement = select(Brainrot)
+        results = session.exec(statement).all()
+        return results
 
 @app.post("/brainrots")
 def add_brainrot(brainrot: Brainrot):
-with Session(engine) as session:
-session.add(brainrot)
-session.commit()
-session.refresh(brainrot)
-return brainrot
+    with Session(engine) as session:
+        session.add(brainrot)
+        session.commit()
+        session.refresh(brainrot)
+        return brainrot
