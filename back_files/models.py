de32598178071394690b01835ce33eb368c55a93
nic340 <nicolediones5@gmail.com>
2025-12-18 07:42:49 -0800

    Update models.py

--- back_files/models.py
+++ back_files/models.py
@@ -5,3 +5,7 @@ class Brainrot(SQLModel, table=True):
     title: str
     content: str
     image: str
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username: str
